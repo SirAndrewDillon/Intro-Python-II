3e63c4bb587c47c8f39f5fba052cd7e9bbced700
Andrew Dillon <majik895@gmail.com>
2019-12-04 10:55:12 -0500

    first commit

--- src/adv.py
+++ src/adv.py
@@ -1,7 +1,10 @@
 from room import Room
+from player import Player
+from item import Item
+import random
 
 # Declare all the rooms
-
+# Dictionary of rooms
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -23,7 +26,8 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-
+# Dot notation to access a class
+# Brackets to access items in a dictionary
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -33,19 +37,103 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
-# Main
-#
+rooms_list = ['outside', 'foyer', 'overlook', 'narrow', 'treasure']
+key_room = random.randint(0, 4)
+block_room = random.randint(0, 4)
+room[rooms_list[key_room]].items = [Item("Key")]
+room[rooms_list[block_room]].items = [Item("Block")]
 
-# Make a new player object that is currently in the 'outside' room.
+player_one = Player(room['outside'], [])
+print("Welcome to the game!", "\n")
+print(str(rooms_list[key_room]), "and", str(rooms_list[block_room]))
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+selection = 0
+
+
+def health():
+    for x in player_one.items:
+        print("****PLAYER ITEMS*****", x.name)
+        if str(x.name) == "Block":
+            print("IF STATEMENT")
+            player_one.health -= 1
+
+
+def food(cur_room):
+    num = random.randint(1, 16)
+    if num == 1:
+        cur_items = room[cur_room].items
+        cur_items.append(Item("Food"))
+        room[cur_room].items = cur_items
+    elif num == 2 or num == 3 or num == 4:
+        cur_items = room[cur_room].items
+        cur_items.append(Item("Food"))
+        room[cur_room].items = cur_items
+
+
+while selection != 'q':
+    print("--------------------------------------", "\n")
+    print(player_one)
+    if len(player_one.current_room.items) > 0:
+        print("To pick up an item, enter: pick up")
+    selection = input(
+        "Which direction would you like to go? (N, E, S, W or Q to quit) ")
+    cur_room = ""
+    for name, desc in room.items():
+        if desc == player_one.current_room:
+            cur_room = name
+
+    try:
+        selection = selection.lower().strip()
+        if selection == 'n':
+            health()
+            player_one.current_room = player_one.current_room.n_to
+            food(cur_room)
+        elif selection == 'e':
+            health()
+            player_one.current_room = player_one.current_room.e_to
+            food(cur_room)
+        elif selection == 's':
+            health()
+            player_one.current_room = player_one.current_room.s_to
+            food(cur_room)
+        elif selection == 'w':
+            health()
+            player_one.current_room = player_one.current_room.w_to
+            food(cur_room)
+        elif len(player_one.current_room.items) > 0 and selection == "pick up":
+            while True:
+                i = 1
+                for x in room[cur_room].items:
+                    output = ""
+                    output += str(i) + ". " + x.name + "\n"
+                    i += 1
+                i = 1
+                print(output)
+                item_selection = input(
+                    "Enter the number for which Item you want to pick up: ")
+                item_selection = int(item_selection)
+                if item_selection > 0 and item_selection <= len(player_one.current_room.items):
+                    item_name = player_one.current_room.items[item_selection-1]
+                    if str(item_name) == "Food":
+                        print("You have gained 2 health!")
+                        health = player_one.health
+                        health += 2
+                        player_one.health = health
+                        room[cur_room].items.remove(item_name)
+                        break
+                    else:
+                        cur_items = player_one.items
+                        cur_items.append(Item(item_name))
+                        player_one.items = cur_items
+                        player_one.current_room.items.remove(item_name)
+                        break
+                else:
+                    print("Please select a valid number")
+        elif selection == 'q':
+            print("Thank for playing!")
+        else:
+            print("--------------------------------------")
+            print("***Please enter one of the 4 Cardinal directions***", "\n")
+    except:
+        print("--------------------------------------")
+        print("***There are no rooms in that direction, please select a different direction***", "\n")
--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# Encapsulation - Every object needs some sort of external facing API on order to manuipulate its internal state
+
+# Inheritance - Allows us to leverage pre existing code that exists on a base case.
+# Explicitly forms an object hierarchy
+
+# Polymorphism
+
+# In Python, you should use dictionaries to store key value pairs.
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
