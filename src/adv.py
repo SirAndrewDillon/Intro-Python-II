a11cc63c513859d0b2a031213d125260c4bbe4f6
Andrew Dillon <majik895@gmail.com>
2019-12-06 08:18:38 -0500

    added files needed and finished adv

--- src/adv.py
+++ src/adv.py
@@ -1,138 +1,120 @@
-import random
-
+import re
+import sys
+import time
+from color import Color
+from item import Item
 from player import Player
 from room import Room
+from textwrap import wrap
 
-# Declare all the rooms
-# Dictionary of rooms
+items = {
+    'key': Item('Key', 'This Key is unusally large, and feels warm to the touch.', useRooms={'Treasure Chamber': f'You hesitate for a moment, contemplating the strange engravings on this golden door. You feel the warmth of the key in your hand as it turns with a satisfying {Color.PURPLE}*CLIK*{Color.END}'}),
+
+    'dogtoy': Item('Rubber Duck', 'It is bright yellow, dripping with dog slobber, and somewhat ripped apart.'),
+
+    'flashlight': Item('Flashlight', 'MagLite Brand. A sturdy piece of metal filled with D cell batteries.', useRooms={'Gravel Pathway': 'You can see a large dog moving beyond the trees. You shine the flashlight directly into his eyes, and he stops for a moment. Suddenly, he lets out a short bark and runs away.', 'Front Lawn of the Mansion': f'You shine the light around the muddy holes in the lawn. A flash of yellow catches your eye. It looks like some kind of dog toy.'}),
+
+    'napkinMap': Item('Napkin', f'It is a map, hastily drawn on a used napkin. You don\'t recognize the symbols, but there are a few letters written in pencil: {Color.RED}W N W N E{Color.END}')
+}
+
 room = {
-    'outside': Room("Outside Cave Entrance",
-                    "North of you, the cave mount beckons"),
+    'outside': Room("Outer Gate", f"To the {Color.PURPLE}north{Color.END} lies a long, unlit, gravel pathway lined with trees. The iron gate to the south is locked, and topped with several layers of razor wire. Somebody is serious about home security.", holding=[items['flashlight']]),
 
-    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'gravel': Room("Gravel Pathway", f"Lawns stretch out to either side, but you cannot see much of anything past the large oak trees lining the pathway. To the {Color.PURPLE}north{Color.END} you can see the shape of a building. As you walk further down the noisy gravel path the shape comes into focus, a decrepit looking white mansion in the colonial style. You can hear something rustling around nearby.", isDark=True),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'frontLawn': Room("Front Lawn of the Mansion", f"{Color.PURPLE}North{Color.END} of you, the door to the mansion is wide open.  What happened here? It is {Color.RED}dark{Color.END}, but you can see several large holes dug into the lawn. You can hear something rustling around nearby.", holding=[items['dogtoy']], isDark=True),
+    
+    'foyer': Room("Foyer", f"This room is mostly empty space surrounding a grand staircase, a few steps are missing but you could climb {Color.PURPLE}up{Color.END}. Dusty passages run {Color.PURPLE}east{Color.END} and {Color.PURPLE}west{Color.END}. "),
 
-    'narrow': Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'ballroom': Room("Grand Ballroom", f"You admire the polished hardwood floors. One of the chandeliers has fallen and radiates crystal shrapnel from the far end of the room. The only exit is {Color.PURPLE}down{Color.END} the staircase.", holding=[items['napkinMap']] ),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'library': Room("Library", f"You stand among thousands of years of collected thoughts. Bookshelves line every wall but they are all emptied. Someone has recently searched this room. Books and furniture lay tossed about in various piles. The only exit is {Color.PURPLE}east{Color.END}."),
+
+    'narrow': Room("Narrow Passage", f"The narrow passage bends here from {Color.PURPLE}west{Color.END} to {Color.PURPLE}north{Color.END}. The smell of lavender permeates the air."),
+
+    'treasure': Room("Treasure Chamber", f"You see a large door, engraved with mysterious symbols. It seems to be made from solid gold, and feels warm to the touch. There is a large [{Color.RED}keyhole{Color.END}] in the center. The only exit is to the {Color.PURPLE}south{Color.END}.", holding=[items['key']]),
 }
 
 # Link rooms together
-# Dot notation to access a class
-# Brackets to access items in a dictionary
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
+room['outside'].n_to = room['gravel']
+room['gravel'].s_to = room['outside']
+
+room['gravel'].s_to = room['outside']
+room['gravel'].n_to = room['frontLawn']
+
+room['frontLawn'].s_to = room['gravel']
+room['frontLawn'].n_to = room['foyer']
+
+room['foyer'].s_to = room['frontLawn']
+room['foyer'].u_to = room['ballroom']
+room['ballroom'].d_to = room['foyer']
+
+room['foyer'].w_to = room['library']
+room['library'].e_to = room['foyer']
+
 room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
+
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-rooms_list = ['outside', 'foyer', 'overlook', 'narrow', 'treasure']
-key_room = random.randint(0, 4)
-block_room = random.randint(0, 4)
-room[rooms_list[key_room]].items = [Item("Key")]
-room[rooms_list[block_room]].items = [Item("Block")]
+# START GAME
+player = Player('Ricky', room['outside'])
+player.startGame()
 
-player_one = Player(room['outside'], [])
-print("Welcome to the game!", "\n")
-print(str(rooms_list[key_room]), "and", str(rooms_list[block_room]))
+# GAMEPLAY LOOP
+while True:
+    player.loc.seen = True
+    location = player.loc.name
+    print(f'You are at the {Color.PURPLE}{location}{Color.END}')
+    
+    youSee = player.loc.showItems()
+    if youSee != None:
+        print(f'You can see {Color.RED}{youSee}{Color.END}')
+    elif player.loc.isDark:
+        print(f'It is {Color.RED}[dark]{Color.END}')
 
-selection = 0
+    myItems = player.showItems()
+    if myItems != None:
+        print(f'You are holding {Color.RED}{myItems}{Color.END}')
+    action = input(f'{Color.GREEN}$ action: {Color.END}')
 
+    dropItem = re.match(r"^drop\s([a-z]*\s?[a-z]*)", action, flags=re.I)
+    if dropItem != None:
+        thisItem = dropItem.group(1)
+        player.drop(thisItem)
+        continue
 
-def health():
-    for x in player_one.items:
-        print("****PLAYER ITEMS*****", x.name)
-        if str(x.name) == "Block":
-            print("IF STATEMENT")
-            player_one.health -= 1
+    useItem = re.match(r"^use\s([a-z]*\s?[a-z]*)", action, flags=re.I)
+    if useItem != None:
+        thisItem = useItem.group(1)
+        player.use(thisItem)
+        continue
 
+    getItem = re.match(r"^get\s([a-z]*\s?[a-z]*)", action, flags=re.I)
+    if getItem != None:
+        thisItem = getItem.group(1)
+        player.loc.getItem(thisItem, player)
+        continue
 
-def food(cur_room):
-    num = random.randint(1, 16)
-    if num == 1:
-        cur_items = room[cur_room].items
-        cur_items.append(Item("Food"))
-        room[cur_room].items = cur_items
-    elif num == 2 or num == 3 or num == 4:
-        cur_items = room[cur_room].items
-        cur_items.append(Item("Food"))
-        room[cur_room].items = cur_items
+    goTo = re.match(r"^go\s([a-z]*)", action, flags=re.I)
+    if goTo != None:
+        there = goTo.group(1).lower()
+        player.go(there)
+        continue
 
-
-while selection != 'q':
-    print("--------------------------------------", "\n")
-    print(player_one)
-    if len(player_one.current_room.items) > 0:
-        print("To pick up an item, enter: pick up")
-    selection = input(
-        "Which direction would you like to go? (N, E, S, W or Q to quit) ")
-    cur_room = ""
-    for name, desc in room.items():
-        if desc == player_one.current_room:
-            cur_room = name
-
-    try:
-        selection = selection.lower().strip()
-        if selection == 'n':
-            health()
-            player_one.current_room = player_one.current_room.n_to
-            food(cur_room)
-        elif selection == 'e':
-            health()
-            player_one.current_room = player_one.current_room.e_to
-            food(cur_room)
-        elif selection == 's':
-            health()
-            player_one.current_room = player_one.current_room.s_to
-            food(cur_room)
-        elif selection == 'w':
-            health()
-            player_one.current_room = player_one.current_room.w_to
-            food(cur_room)
-        elif len(player_one.current_room.items) > 0 and selection == "pick up":
-            while True:
-                i = 1
-                for x in room[cur_room].items:
-                    output = ""
-                    output += str(i) + ". " + x.name + "\n"
-                    i += 1
-                i = 1
-                print(output)
-                item_selection = input(
-                    "Enter the number for which Item you want to pick up: ")
-                item_selection = int(item_selection)
-                if item_selection > 0 and item_selection <= len(player_one.current_room.items):
-                    item_name = player_one.current_room.items[item_selection - 1]
-                    if str(item_name) == "Food":
-                        print("You have gained 2 health!")
-                        health = player_one.health
-                        health += 2
-                        player_one.health = health
-                        room[cur_room].items.remove(item_name)
-                        break
-                    else:
-                        cur_items = player_one.items
-                        cur_items.append(Item(item_name))
-                        player_one.items = cur_items
-                        player_one.current_room.items.remove(item_name)
-                        break
-                else:
-                    print("Please select a valid number")
-        elif selection == 'q':
-            print("Thank for playing!")
-        else:
-            print("--------------------------------------")
-            print("***Please enter one of the 4 Cardinal directions***", "\n")
-    except:
-        print("--------------------------------------")
-        print("***There are no rooms in that direction, please select a different direction***", "\n")
+    if action == 'help':
+        player.showHelp()
+        continue
+    if action == 'look':
+        print('\n')
+        print(player.loc)
+        continue
+    if action == 'q':
+        player.quitGame()
+        break
+    else:
+        print('\n')
+        print(f'{action} {Color.RED}action not recognized{Color.END}\ntype {Color.PURPLE}help{Color.END} to see a list of actions')
+        print('\n')
+        continue
